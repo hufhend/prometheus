42b72251bc50ec6428165d8792362cf09135ac14
Václav Dvorský <hufhendr@gmail.com>
2024-04-03 23:03:04 +0200

    Installing Docker differently

--- init.sh
+++ init.sh
@@ -34,23 +34,6 @@ if ! [ $(id -u) = 0 ]; then
     sudo chown -R 472:root grafana/data
     sudo chown nobody:nogroup alertmanager/data
 
-    # docker installation
-    # sudo apt-get -f install && sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
-    # sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    # sudo apt-get update
-    # sudo apt install -y docker-ce docker-ce-cli containerd.io
-    # sudo usermod -aG docker ${USER}
-    # echo "The environment is ready, Docker is installed, proceed through the GitLab"
-
-    # uncomment for manual installation, we use the last version docker-compose before v2
-    # sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    # sudo chmod +x /usr/local/bin/docker-compose
-    # sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-    # sudo apt-get install -y sysstat && sudo systemctl enable sysstat && sudo sed -i 's/false/true/g' /etc/default/sysstat
-    # id -nG && systemctl is-enabled docker && docker-compose --version
-
   exit 0
 fi
     echo "Do not run this script as root"
